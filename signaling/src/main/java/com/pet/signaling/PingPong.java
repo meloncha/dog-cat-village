0d6f8a919877490298223a2b8219b496c72943eb
박성호 <solser12@naver.com>
2021-04-09 09:06:02 +0900

    test

--- signaling/src/main/java/com/pet/signaling/PingPong.java
+++ signaling/src/main/java/com/pet/signaling/PingPong.java
@@ -1,15 +1,19 @@
 package com.pet.signaling;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PingPong implements Runnable {
 
     @Override
     public void run() {
-        try {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (true) {
+            try {
+                log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 }
